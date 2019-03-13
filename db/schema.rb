795e70869f6bbd256bba7dc8364d0bf68242c131
Elyse Burmester <burmesterelyse@gmail.com>
2019-03-13 20:43:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201201114) do
+ActiveRecord::Schema.define(version: 20190305164323) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.string "content"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
     t.string "content"
   end
 
